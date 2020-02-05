939c374267b7b072268d91bdeda73fc900cead3b
vincenzovolpe <vinci792010@gmail.com>
2020-02-05 20:10:26 +0100

    Aggiunti commenti nel file test

--- test.php
+++ test.php
@@ -4,21 +4,20 @@ include_once 'Prodotto.php';
 include_once 'Telefono.php';
 include_once 'Computer.php';
 
+// Creo un oggetto istanziando la classe prodotto
 $prodotto_generico = new Prodotto('Borsa Computer', 100, 50, 22);
-
+// Creo un oggetto istanziando la classe computer che estende la classe prodotto
 $computer = new Computer('HP Pavilion', 1000, 600, 22, 2.4, 8);
-
+// Creo un oggetto istanziando la classe telefono che estende la classe prodotto
 $telefono = new Telefono('Galaxy Note', 900, 550, 22, 'Samsung', 'Android');
-
+// Creo uno sconto per l'oggetto prodotto
 $prodotto_generico->percSconto = 20;
-
+// Creo uno sconto per l'oggetto computer
 $computer->percSconto = 50;
-
+// Creo uno sconto per l'oggetto telefono
 $telefono->percSconto = 30;
 
-
-//$telefono = new telefono();
-
+// Stampo il prodotto
 echo "Primo prodotto generico in magazzino";
 echo '<br>';
 echo "Nome Prodotto: " . $prodotto_generico->nome;
@@ -29,6 +28,7 @@ echo "Prezzo scontato prodotto: € " .$prodotto_generico->getPrezzoScontato();
 echo '<br>';
 echo "----------------------------";
 echo '<br>';
+// Stampo il computer
 echo "Primo computer in magazzino";
 echo '<br>';
 echo "Nome Computer: " . $computer->nome;
@@ -43,13 +43,14 @@ echo "Memoria RAM: " .$computer->ram ." GB";
 echo '<br>';
 echo "----------------------------";
 echo '<br>';
+// Stampo il telefono
 echo "Primo telefono in magazzino";
 echo '<br>';
 echo "Nome Telefono: " . $telefono->nome;
 echo '<br>';
 echo "Prezzo di vendita telefono: € " .$telefono->getPrezzoInteroIvato();
 echo '<br>';
-echo "Prezzo scontato computer: € " .$telefono->getPrezzoScontato();
+echo "Prezzo scontato telefono: € " .$telefono->getPrezzoScontato();
 echo '<br>';
 echo "Brand Telefono: " .$telefono->brand;
 echo '<br>';
